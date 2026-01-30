448047248eb4f611f9ccdc68a007d778228d890c
SubHunt <phaeton.kz@gmail.com>
2026-01-30 16:55:13 +0300

    send Form to server

---
+++ js/portfolio/js/sendForm.js
@@ -0,0 +1,37 @@
+const form = document.querySelector(".form-test-drive");
+const forms = document.querySelectorAll(".form");
+
+forms.forEach((form) => {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const formData = new FormData(form);
+        const body = {};
+        formData.append("form", form.classList.value);
+        formData.forEach((value, field) => {
+            body[field] = value;
+        });
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error(response.status);
+                }
+            })
+            .then((data) => {
+                alert("Данные отправлены успешно");
+            })
+            .catch((error) => {
+                alert("Ошибка при отправке данных: " + error.message);
+            })
+            .finally(() => {
+                form.reset();
+            });
+    });
+});
