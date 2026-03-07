0bcc37bff165ddc861b3a4bbdc2286908bfbf252
SubHunt <phaeton.kz@gmail.com>
2026-03-07 13:50:44 +0300

    slider/swipper

--- anime/js/bg_elements.js
+++ anime/js/bg_elements.js
@@ -1,6 +1,10 @@
-const elements = document.querySelectorAll(".set-bg");
+const bgElements = () => {
+    const elements = document.querySelectorAll(".set-bg");
 
-for (let i = 0; i < elements.length; i++) {
-    elements[i].style.backgroundImage =
-        `url(${elements[i].getAttribute("data-setbg")})`;
-}
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundImage =
+            `url(${elements[i].getAttribute("data-setbg")})`;
+    }
+};
+
+bgElements();
