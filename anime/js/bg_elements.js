61142d604848ee7b41efcf05eda3ea1bf44e8c0d
Sergey Matofonov <phaeton.kz@gmail.com>
2026-03-10 08:54:08 +0300

    anime work json

--- anime/js/bg_elements.js
+++ anime/js/bg_elements.js
@@ -1,10 +1,14 @@
 const bgElements = () => {
-    const elements = document.querySelectorAll(".set-bg");
+  const elements = document.querySelectorAll(".set-bg");
 
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].style.backgroundImage =
-            `url(${elements[i].getAttribute("data-setbg")})`;
-    }
+  elements.forEach((elem) => {
+    elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+  });
+
+  // for (let i = 0; i < elements.length; i++) {
+  //     elements[i].style.backgroundImage =
+  //         `url(${elements[i].getAttribute("data-setbg")})`;
+  // }
 };
 
 bgElements();
