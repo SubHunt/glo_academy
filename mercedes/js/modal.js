513f3e4362a77e2a08b676c436dc0e729e284723
SubHunt <phaeton.kz@gmail.com>
2026-04-10 17:40:07 +0300

    Add burger menu and modal window

---
+++ mercedes/js/modal.js
@@ -0,0 +1,15 @@
+const moreBtn = document.querySelector(".more");
+const modal = document.querySelector(".modal");
+moreBtn.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+});
+
+modal.addEventListener("click", (event) => {
+    target = event.target;
+    if (
+        target.classList.contains("overlay") ||
+        target.classList.contains("modal__close")
+    ) {
+        modal.classList.add("hidden");
+    }
+});
