98523935f908ce269592f852333fab2eae558d8b
SubHunt <phaeton.kz@gmail.com>
2026-04-11 17:04:44 +0300

    fetch

---
+++ mercedes/js/sendForm.js
@@ -0,0 +1,12 @@
+const form = document.querySelector(".form-test-drive");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let data = {};
+
+    for (let { name, value } of form.elements) {
+        if (name) {
+            data[name] = value;
+        }
+    }
+});
