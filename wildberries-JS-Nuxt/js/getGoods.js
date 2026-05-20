605c49aaa23193e48a5e9727948ba929e6fe0c44
SubHunt <phaeton.kz@gmail.com>
2026-05-20 23:02:12 +0300

    Cart manager

--- wildberries-JS-Nuxt/js/getGoods.js
+++ wildberries-JS-Nuxt/js/getGoods.js
@@ -1,5 +1,6 @@
 const getGoods = () => {
     const links = document.querySelectorAll(".navigation-link");
+    const more = document.querySelector(".more");
 
     const renderGoods = (goods) => {
         const goodsContainer = document.querySelector(".long-goods-list");
@@ -59,6 +60,13 @@ const getGoods = () => {
         window.location.pathname === "/goods.html"
     ) {
         renderGoods(JSON.parse(localStorage.getItem("goods")));
+    }
+
+    if (more) {
+        more.addEventListener("click", () => {
+            event.preventDefault();
+            getData();
+        });
     }
 };
 
